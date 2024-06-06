5f074c7f7ace3e8dfec8fc822a5ecc6daf250eea
sarahEl3anany <sarah.3anany@gmail.com>
2024-06-06 17:25:06 +0300

    read only off

--- front-end/pages/login.vue
+++ front-end/pages/login.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="mb-5">
         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Your Password</label>
-        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500"/>
+        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required/>
       </div>
     </form>
   </template>
