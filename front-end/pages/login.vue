107d8ae2aa3600f497bcd971e4460f2cf74e9392
Sarah <sarah.3anany@gmail.com>
2024-06-22 13:25:55 +0300

    align

--- front-end/pages/login.vue
+++ front-end/pages/login.vue
@@ -17,9 +17,11 @@
           Remember me
         </label>
       </div>
-      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+      <div class="flex flex-col items-center justify-center">
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
         Login
       </button>
+      </div>
     </form>
   </div>
 </template>
