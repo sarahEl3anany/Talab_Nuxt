ef87747e978b1af3cbe7205360580680e1b03869
Sarah El3anany <sarah.3anany@gmail.com>
2024-07-02 12:01:43 +0300

    google auth

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -6,7 +6,10 @@ export default defineNuxtConfig({
       baseURL: process.env.BASE_URL || 'http://127.0.0.1:8000',
     },
   },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/image"],
+  modules: ['@nuxtjs/tailwindcss', "@nuxt/image", 'nuxt-vue3-google-signin'],
+  googleSignIn: {
+    clientId: '812442679665-j0oi4lse4s5gugt6pi9plr44akd2pris.apps.googleusercontent.com',
+  },
   css:[
     '@fortawesome/fontawesome-svg-core/styles.css'
   ],
--- front-end/pages/login.vue
+++ front-end/pages/login.vue
@@ -1,39 +1,62 @@
 <template>
-  <div class="block align-middle my-12 max-w-sm mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+  <div
+    class="block align-middle my-12 max-w-sm mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
     <form>
-      <div class="mb-5"> 
+      <div class="mb-5">
         <label for="email" class="block mb-2 text-sm font-medium text-blue-700">Your Email</label>
-        <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@domain.com" required />
+        <input type="email" id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="name@domain.com" required />
       </div>
       <div class="mb-5">
-        <label for="password" class="block mb-2 text-sm font-medium text-blue-700" >Your Password</label>
-        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Password" required/>
+        <label for="password" class="block mb-2 text-sm font-medium text-blue-700">Your Password</label>
+        <input type="password" id="password"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="Password" required />
       </div>
       <div class="flex items-start mb-5">
         <div class="flex items-center h-5">
-          <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" />
+          <input id="remember" type="checkbox" value=""
+            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" />
         </div>
         <label for="remember" class="ms-2 text-sm font-medium text-blue-700">
           Remember me
         </label>
       </div>
       <div class="flex flex-col items-center justify-center">
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-        Login
-      </button>
+        <button type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+          Login
+        </button>
+      </div>
+      <div>
+        <GoogleSignInButton @success="handleLoginSuccess" @error="handleLoginError"></GoogleSignInButton>
       </div>
     </form>
   </div>
 </template>
-  
-  <script>
-  export default {
-    name: 'LoginPage',
-    methods:{
-      loginSubmit(){
-        
-      }
+
+<script lang="ts">
+import {
+  GoogleSignInButton,
+  type CredentialResponse,
+} from "vue3-google-signin";
+
+export default {
+  name: 'LoginPage',
+  methods: {
+    handleLoginSuccess(response: CredentialResponse) {
+      const { credential } = response;
+      debugger
+      console.log("Access Token", credential);
+    },
+    handleLoginError() {
+      console.error("Login failed");
+
+    },
+    loginSubmit() {
+
     }
   }
-  </script>
-  
+}
+</script>
